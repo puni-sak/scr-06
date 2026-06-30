8145e71315291d02ed1e6ee9d9cebdcdcbd4c589
puni-sak <punicco2008@gmail.com>
2026-07-01 00:00:13 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,147 @@
 (() => {
-window.addEventListener('load', function(){
 
-// フルスクリーン表示
-document.getElementById('button1').addEventListener('click', function(){
+//問題を変える
+let mondaibun1 = [
+  '問題文１',
+  '問題文２',
+  '問題文３',
+  '問題文４',
+  '問題文５',
+  '問題文６',
+  '問題文７',
+  '問題文８',
+  '問題文９',
+  '問題文１０'
+];
 
-// Chrome & Firefox v64以降
-if( document.body.requestFullscreen ) {
- document.body.requestFullscreen();
+let mondaibun2 = [
+  '問題文１',
+  '問題文２',
+  '問題文３',
+  '問題文４',
+  '問題文５',
+  '問題文６',
+  '問題文７',
+  '問題文８',
+  '問題文９',
+  '問題文１０'
+];
 
-// Safari & Edge & Chrome v68以前
-} else if( document.body.webkitRequestFullscreen ) {
-  document.body.webkitRequestFullscreen();
+let mondaibun3 = [
+  '問題文１',
+  '問題文２',
+  '問題文３',
+  '問題文４',
+  '問題文５',
+  '問題文６',
+  '問題文７',
+  '問題文８',
+  '問題文９',
+  '問題文１０'
+];
+
+let mondaibun4 = [
+  '問題文１',
+  '問題文２',
+  '問題文３',
+  '問題文４',
+  '問題文５',
+  '問題文６',
+  '問題文７',
+  '問題文８',
+  '問題文９',
+  '問題文１０'
+];
+
+let mondaibun5 = [
+  '問題文１',
+  '問題文２',
+  '問題文３',
+  '問題文４',
+  '問題文５',
+  '問題文６',
+  '問題文７',
+  '問題文８',
+  '問題文９',
+  '問題文１０'
+];
+
+let mondaibun6 = [
+  '問題文１',
+  '問題文２',
+  '問題文３',
+  '問題文４',
+  '問題文５',
+  '問題文６',
+  '問題文７',
+  '問題文８',
+  '問題文９',
+  '問題文１０'
+];
+
+let mondaibun7 = [
+  '問題文１',
+  '問題文２',
+  '問題文３',
+  '問題文４',
+  '問題文５',
+  '問題文６',
+  '問題文７',
+  '問題文８',
+  '問題文９',
+  '問題文１０'
+];
+
+let mondaibun8 = [
+  '問題文１',
+  '問題文２',
+  '問題文３',
+  '問題文４',
+  '問題文５',
+  '問題文６',
+  '問題文７',
+  '問題文８',
+  '問題文９',
+  '問題文１０'
+];
+
+//タイトル(問題セットの番号)を取得
+const title = document.title;
+let setbangou = Number( title );
+let mondaibun = [];
+
+if(setbangou == 1) {
+  mondaibun = mondaibun1;
 }
-});
-
-// フルスクリーン解除
-document.getElementById('button2').addEventListener('click', function(){
-
-// Chrome & Firefox v64以降
-if( document.exitFullscreen ) {
-  document.exitFullscreen();
-
-// Safari & Edge & Chrome v44以前
-} else if( document.webkitCancelFullScreen ) {
-  document.webkitCancelFullScreen();
+else if(setbangou == 2) {
+  mondaibun = mondaibun2;
 }
-});
-});
+else if(setbangou == 3) {
+  mondaibun = mondaibun3;
+}
+else if(setbangou == 4) {
+  mondaibun = mondaibun4;
+}
+else if(setbangou == 5) {
+  mondaibun = mondaibun5;
+}
+else if(setbangou == 6) {
+  mondaibun = mondaibun6;
+}
+else if(setbangou == 7) {
+  mondaibun = mondaibun7;
+}
+else{
+  mondaibun = mondaibun8;
+}
+
+let mondaibangou = 0;
+
+document.getElementById("mondaibun").innerHTML = mondaibun[0];
 
 //答える問題を選択
-// 要素を取得
 const choose = document.querySelectorAll(".choose");
 
-// 一括でイベントリスナ
 choose.forEach((chosing) => {
 
     chosing.addEventListener("click", () => {
@@ -43,17 +152,14 @@ choose.forEach((chosing) => {
         t.classList.remove("chose");
       });
     }
-      chosing.classList.add('chose');
+    chosing.classList.add('chose');
     });
 });
 
-
-let ok = document.getElementById('ok');
-let ng = document.getElementById('ng');
-let gameover = document.getElementById('gameover');
+//キーボード操作
 const maru = document.getElementById('maru');
 const batsu = document.getElementById('batsu');
-const gameoverImg = document.getElementById('gameoverImg');
+const gameover = document.getElementById('gameover');
 const clear = document.getElementById('clear');
 
 let maruKesu = function() {
@@ -64,34 +170,92 @@ let batsuKesu = function() {
   batsu.classList.remove("mieru");
   batsu.classList.add("mienai");
 }
-let clearhyozi = function() {
-  clear.classList.remove("mienai");
-  clear.classList.add("mieru");
-}
 
-ok.addEventListener('click', function() {
-  const chose = document.getElementsByClassName("chose");
-  maru.classList.remove("mienai");
-  maru.classList.add("mieru");
-  //chose[0].classList.remove("chose");
-  chose[0].classList.remove("choose");
-  chose[0].classList.add("noChoose");
-  setTimeout(maruKesu, 1000);
-  const noChoose = document.getElementsByClassName("noChoose");
-  if(noChoose.length === 10) {
-    setTimeout(clearhyozi, 2000);
+//キーが押されたら
+document.addEventListener('keydown', event => {
+
+  //1が押されたらマル
+  if(event.key == "1") {
+    const chose = document.getElementsByClassName("chose");
+    maru.classList.remove("mienai");
+    maru.classList.add("mieru");
+    setTimeout(maruKesu, 1000);
+    chose[0].classList.remove("choose");
+    chose[0].classList.add("noChoose");
+  }
+
+  //2が押されたらバツ
+  if(event.key == "2") {
+    batsu.classList.remove("mienai");
+    batsu.classList.add("mieru");
+    setTimeout(batsuKesu, 1000);
+  }
+
+  //3が押されたらクリア
+  if(event.key == "3") {
+    clear.classList.remove("mienai");
+    clear.classList.add("mieru");
+    let bg = mondaibangou;
+  }
+
+  //4が押されたらゲームオーバー
+  if(event.key == "4") {
+    gameover.classList.remove("mienai");
+    gameover.classList.add("mieru");
+  }
+
+  //Enterが押されたら次の問題へ
+  if(event.key == "Enter") {
+    mondaibangou = mondaibangou + 1;
+
+    //10問目なら終わり
+    if(mondaibangou == mondaibun.length) {
+      window.open("../../index.html", "_self");
+    }
+    //問題文・画像を次の問題用に変更
+    else {
+      document.getElementById("mondaibun").innerHTML = mondaibun[mondaibangou];
+      document.getElementById("a").src = `img${mondaibangou + 1}/a.jpg`;
+      document.getElementById("b").src = `img${mondaibangou + 1}/b.jpg`;
+      document.getElementById("c").src = `img${mondaibangou + 1}/c.jpg`;
+      document.getElementById("d").src = `img${mondaibangou + 1}/d.jpg`;
+      document.getElementById("e").src = `img${mondaibangou + 1}/e.jpg`;
+      document.getElementById("f").src = `img${mondaibangou + 1}/f.jpg`;
+      document.getElementById("g").src = `img${mondaibangou + 1}/g.jpg`;
+      document.getElementById("h").src = `img${mondaibangou + 1}/h.jpg`;
+      document.getElementById("i").src = `img${mondaibangou + 1}/i.jpg`;
+      document.getElementById("j").src = `img${mondaibangou + 1}/j.jpg`;
+
+      //初期化
+      choose.forEach((t) => {
+        t.classList.remove("noChoose");
+        t.classList.remove("chose");
+        console.log(choose);
+      });
+    }
+
+    clear.classList.remove("mieru");
+    clear.classList.add("mienai");
+    gameover.classList.remove("mieru");
+    gameover.classList.add("mienai");
   }
 }, false);
 
-ng.addEventListener('click', function() {
-  batsu.classList.remove("mienai");
-  batsu.classList.add("mieru");
-  setTimeout(batsuKesu, 1000);
-}, false);
+//画像がないとき
+window.addEventListener("DOMContentLoaded", () => {
+  const imgs = document.querySelectorAll("img");
 
-gameover.addEventListener('click', function() {
-  gameoverImg.classList.remove("mienai");
-  gameoverImg.classList.add("mieru");
-}, false);
+  imgs.forEach(img => {
+    img.onerror = () => {
+      img.style.display = "none";   // 読み込み失敗 → 非表示
+    };
+
+    const observer = new MutationObserver(() => {
+      img.style.display = "";
+    });
+
+    observer.observe(img, { attributes: true, attributeFilter: ["src"]});
+  });
+});
 
 })();
